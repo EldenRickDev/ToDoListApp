d3415c98baa269e61608c008ac8e24aa8c7feaf4
EldenRickDev <eldenrickdev@gmail.com>
2024-08-21 12:14:54 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Ricardo Andrés Gatica Collarte on 21-08-24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
